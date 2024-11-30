867cda4a4612527cc896086ea80b241011d6593e
genki.sano <g.sano.2441@gmail.com>
2024-11-30 20:21:43 +0900

    update: 12章のコードを完成形に修正する

--- app/chapter12/app/FizzBuzz.php
+++ app/chapter12/app/FizzBuzz.php
@@ -6,6 +6,15 @@ class FizzBuzz
 {
     public function execute(int $number): string
     {
-        return $number;
+        if ($number % 3 === 0) {
+            if ($number % 5 === 0) {
+                return 'FizzBuzz';
+            }
+            return 'Fizz';
+        }
+        if ($number % 5 === 0) {
+            return 'Buzz';
+        }
+        return (string) $number;
     }
 }
